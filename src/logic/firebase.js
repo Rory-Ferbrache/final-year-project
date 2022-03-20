2e2b52f47bf1e86977bcc6a42640d8350cd11b76
Rory-Ferbrache <rory.ferbrache@outlook.com>
2022-03-20 15:35:31 +0000

    Complete firebase integration

--- src/logic/firebase.js
+++ src/logic/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
 
 export function AuthCreateUserWithEmailAndPassword(email,password,confirmPassword){
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
+import { AuthSignOut, auth } from '../logic/firebase'
+import { getAuth } from "firebase/auth"
+
+
 
 function DashboardPage() {
+  const navigation = useNavigate()
+  const user = auth.currentUser
   return (
     <>
       <h1>Dashboard</h1>
+      {user ? <button onClick={AuthSignOut}>log out</button> : <button onClick={() => navigation("/sign-up")}>login</button>}
     </>
   )
 }
